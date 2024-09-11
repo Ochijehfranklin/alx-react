8c5002db8aec3cc48a250846e8a1b403e5cce854
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-09-11 18:38:27 +0100

    Converting into an Immutable object using fromJS

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,8 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    const immutableObject = fromJS(object);
-    
-    return immutableObject.getIn(array, undefined);
+    const mappedObj = fromJS(object);
+
+    return mappedObj.getIn(array, undefined);
 }
+
